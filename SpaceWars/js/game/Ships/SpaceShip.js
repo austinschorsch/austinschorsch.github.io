2482a58fe601e4634fe2513eca2abe6885ede6d9
Austin Schorsch <austin.schorsch@gmail.com>
2016-04-12 13:22:48 -0700

    small changes

--- SpaceWars/js/game/Ships/SpaceShip.js
+++ SpaceWars/js/game/Ships/SpaceShip.js
@@ -10,13 +10,13 @@ function SpaceShip(context, image, imageIndex, imageOffset, width, height)
     var speed = 7;              // SUBJECT TO CHANGE - PLAYER SPEED
     var keys = [];
     var loop = 0;
-    
+
     self.timer = 0;
     self.bombsCount = 3;        // Keeps track of number of bombs left
     self.bullets = Array();
     self.health = 68;
-    self.dead = false; 
-    
+    self.dead = false;
+
     self.image = image;
     self.imageIndex = imageIndex;
     self.imageOffset = imageOffset;
@@ -24,7 +24,7 @@ function SpaceShip(context, image, imageIndex, imageOffset, width, height)
     self.height = height;
     self.x = (window.innerWidth * .75) / 2 - 30;
     self.y = (window.innerHeight * .9) + 700;
-    
+
     self.render = function()
     {
         self.context.drawImage(self.image,       //source image
@@ -70,19 +70,19 @@ function SpaceShip(context, image, imageIndex, imageOffset, width, height)
         // Check VERTICAL bounds
         if (self.y >= (window.innerHeight * .9 - 70)) {
             self.y = window.innerHeight * .9 - 70;
-        } else if (self.y <= 450) {
-            self.y = 450;
+        } else if (self.y <= window.innerHeight / 1.75) {
+            self.y = window.innerHeight / 1.75;
         }
         setTimeout(moveShip, 15);
     }
-        
+
     moveShip();
 
     function fireBullet() {
-        
-        if (keys[32]) 
+
+        if (keys[32])
         {
-            var tempBullet = playerBullet.makeBullet(context, 0, self.x + self.width / 2 - 10, self.y); 
+            var tempBullet = playerBullet.makeBullet(context, 0, self.x + self.width / 2 - 10, self.y);
             tempBullet.init();
             self.bullets.push(tempBullet);
         }
@@ -93,62 +93,62 @@ function SpaceShip(context, image, imageIndex, imageOffset, width, height)
                 var tempBullet = playerBullet.makeBullet(context, 1, self.x + self.width / 2 - 12, self.y);
                 tempBullet.init();
                 self.bullets.push(tempBullet);
-                
+
                 self.bombsCount--;
                 if (self.bombsCount == 0) {
                     timeout = setTimeout(bombsReset, 10000);
                 }
-                
+
                 document.getElementById("bomb-count").innerHTML = self.bombsCount;
             }
             // Or we are out of bombs
             else {
                 document.getElementById("special-bullets").style.color = "red";
                 document.getElementById("bomb-count").style.color = "red";
-                
+
                 setTimeout(flash, 300);
             }
-        }           
+        }
     }
-    
+
     function resetTimer() {
         self.timer = 0;
     };
-    
-    // This function makes the ship have a "blink"-like animation to 
+
+    // This function makes the ship have a "blink"-like animation to
     // show that the ship has taken damage
     self.blink = function ()
     {
         if (loop === 0)
         {
-            self.image.src = "images/ships/ships-blink.png";              //destination height (for scaling)   
+            self.image.src = "images/ships/ships-blink.png";              //destination height (for scaling)
             loop = 1;
 
             setTimeout(self.blink, 120);
         }
-        else 
+        else
         {
             self.image.src = "images/ships/ships.png";
             loop = 0;
         }
     };
-    
+
     // Make the ship have an explosion-like animation upon death
     self.explode = function ()
     {
         self.image.src = "images/widgets/explosion.png";
         self.height = 150;
         self.dead = true;
-        
-        if (loop < 2) 
+
+        if (loop < 2)
         {
-            self.width = 120; 
-            self.imageOffset = loop * self.width; 
+            self.width = 120;
+            self.imageOffset = loop * self.width;
             self.imageIndex = loop;
         }
         else if (loop >= 2 && loop < 2.5)
         {
-            self.width = 130;   
+            self.width = 130;
             self.imageOffset = loop * self.width;
             self.imageIndex = loop;
         }
@@ -160,7 +160,7 @@ function SpaceShip(context, image, imageIndex, imageOffset, width, height)
         }
         else if (loop >= 2.5 && loop < 3.5)
         {
-            self.width = 110; 
+            self.width = 110;
             self.imageOffset = loop * self.width - 30;
             self.imageIndex = loop;
         }
@@ -170,16 +170,16 @@ function SpaceShip(context, image, imageIndex, imageOffset, width, height)
             self.imageOffset = loop * self.width + 60;
             self.imageIndex = loop;
         }
-        
+
         loop += .5;
         setTimeout(self.explode, 30);
     };
-    
+
     function bombsReset() {
         if (self.bombsCount < 3) {
             self.bombsCount++;
             updateLabels(0);
-            
+
             // Reset Timeout
             setTimeout(bombsReset, 300);
         }
@@ -187,7 +187,7 @@ function SpaceShip(context, image, imageIndex, imageOffset, width, height)
             updateLabels(1);
         }
     };
-    
+
     function updateLabels(style) {
         if (style == 0) {
             document.getElementById("bomb-count").innerHTML = self.bombsCount;
@@ -199,30 +199,30 @@ function SpaceShip(context, image, imageIndex, imageOffset, width, height)
             document.getElementById("bomb-count").style.color = "darkgoldenrod";
         }
     };
-    
+
     // Bullet count flash effect
     function flash() {
         document.getElementById("special-bullets").style.color = "white";
         document.getElementById("bomb-count").style.color = "darkgoldenrod";
     }
-    
+
     document.addEventListener("keydown", function (e) {
         keys[e.keyCode] = true;
     });
-    
+
     document.addEventListener("keyup", function (e) {
         // Reset X Velocity
         velocityX = 0;
-        
+
         if (keys[32] || keys[13]) {
             if (self.timer <= 0) {
                 fireBullet();
-                
+
                 // Update the timer
                 self.timer = 20;
             }
         }
-        
+
         keys[e.keyCode] = false;
     });
 }
